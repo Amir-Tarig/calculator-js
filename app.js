68b62fe7ba4e769ec9ec918fac8509134d0bfdfb
Miro <MiroPhoenix91@gmail.com>
2021-08-16 23:57:29 +0200

    populating the display screen when the user click the numbers button

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 function calculator () {
+    const perviousText = document.querySelector('.previous')
+    const currentText = document.querySelector('.current')
+    const Numbuttons = Array.from(document.querySelectorAll('.num'))
+    const OperationsButton = document.querySelectorAll('.operation')
+    const deleteButton = document.querySelector('.delete')
+    const clearButton = document.querySelector('.clear')
+    let currentOperand = ''
+    let previousOperand = ''
+
+    // console.log(perviousText)
+    // console.log(currentText)
+    // console.log(Numbuttons)
+    // console.log(OperationsButton)
+    // console.log(deleteButton)
+    // console.log(clearButton)
 
     const add = (x , y) => {  return  x   +   y } 
     const subtract = (x , y) => {  return x   -   y }
     const multiply = (x , y) => {  return x   *  y  } 
     const divide = (x , y) => {  return x   /  y}  
 
-    function operate(x , y) {
-        console.log(divide(x,y))
-        console.log(subtract(x,y))
-        console.log(multiply(x,y))
-        console.log(add(x,y))
+    function operate(operator, x, y) {
+        
     }
 
-    operate(4,2)
+    //function to populate the display when the user click the numbers buttons
+    function populateDisplay() {
+        Numbuttons.map(btn =>{
+            btn.addEventListener('click', () => {
+                currentOperand = currentOperand.toString() + btn.textContent.toString()
+                currentText.innerText = currentOperand
+            })
+        })
+    }
+
+    populateDisplay()
 }
 calculator()
 
